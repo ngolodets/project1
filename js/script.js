4accd52d2777dfd7d6476bd4a4e30aa774b543f6
Nina Golodets <ninagolodets@Ninas-MacBook-Pro.local>
2019-06-12 11:20:04 -0700

    added win/lose logic

--- js/script.js
+++ js/script.js
@@ -4,9 +4,12 @@ var easyWin = 0;
 var easyLoss = 0;
 var gameOver = false;
 var easyRemainingTime = 0;
-var delayHandle;
+var timeLeft = 0;
+var delayHandle = null;
+var timerHandle = null;
+//var easyTimerHandle = null;
 //var easyDelayHandle = null;
-var easyTimerHandle = null;
+
 //var picsFlipped = 0;
 //picsInPlayEasy = [];
 
@@ -160,12 +163,12 @@ var easyPicsGuessed = {
 
 var openedPicsEasy =[];
 var guessedPicsEasy = [];
-var notGuessedPicsEasy = [];
+//var notGuessedPicsEasy = [];
 //var openedPicsEasyObj = {};
 //var guessedPicsEasy = [];
-var picIdEasy;
-var count = 0;
-var picsOpened = 0;
+//var picIdEasy;
+//var count = 0;
+//var picsOpened = 0;
 
 //DOM References:
 var easyTimerText;
@@ -184,16 +187,17 @@ document.addEventListener("DOMContentLoaded", function(e) {
   gridBoxEasy = document.getElementById("gridboxeasy");
 
 
-  //initGame();
+  initGame();
 
   // Click handlers
   easyStartGameBttn.addEventListener("click", function(e) {
     console.log("clicked start game");
-    //easyTimerHandle = setInterval(updateClock, 1000);
+    timerHandle = setInterval(updateClock, 1000);
+    initGame();
   });
   easyPlayNewGameBttn.addEventListener("click", function(e) {
     console.log("clicked play new game");
-    //reset();
+    reset();
   });
   gridBoxEasy.addEventListener("click", function(e) {
     if (!easyPicsGuessed[e.target.id] && !gameOver) {
@@ -203,13 +207,12 @@ document.addEventListener("DOMContentLoaded", function(e) {
       openedPicsEasy.push(e.target.id);
       
       if (openedPicsEasy.length === 2 && currentBoardEasy[openedPicsEasy[0]].letter === currentBoardEasy[openedPicsEasy[1]].letter) { //openedPicsEasy[0] === openedPicsEasy[1]) {
+        //matched
         console.log("found a match!");
   
         guessedPicsEasy.push(openedPicsEasy);
         openedPicsEasy = [];
         
-        
-        console.log("guessed pics: " + openedPicsEasy);
           //openedPicsEasy.push(currentBoardEasy[e.target.id].letter);
           //if (guessedPicsEasy.length === currentBoardEasy.length) {
           //}
@@ -217,33 +220,190 @@ document.addEventListener("DOMContentLoaded", function(e) {
           //openedPicsEasy = [];
         console.log("opened pics are: " + openedPicsEasy);
         console.log("guessed pics are: " + guessedPicsEasy);
-          //console.log("opened pics " + openedPicsEasy.length);
+        //checkWin();
 
       } else if (openedPicsEasy.length === 2 && currentBoardEasy[openedPicsEasy[0]].letter !== currentBoardEasy[openedPicsEasy[1]].letter) {
           //unmatched
           console.log("no match!")
           console.log("My array", currentBoardEasy);
           console.log("Opened array", openedPicsEasy);
-          // openedPicsEasy.pop(currentBoardEasy[e.target.id].letter);
-          // openedPicsEasy.pop(currentBoardEasy[e.target.id].letter);
-
-          
+        // openedPicsEasy.pop(currentBoardEasy[e.target.id].letter);
+        // openedPicsEasy.pop(currentBoardEasy[e.target.id].letter);
           delayHandle = setTimeout(function() {
-            // e.target.src = currentBoardEasy[e.target.id].imageback;
-            document.getElementById(openedPicsEasy[0]).src = currentBoardEasy[openedPicsEasy[0]].imageback;
-            document.getElementById(openedPicsEasy[1]).src = currentBoardEasy[openedPicsEasy[1]].imageback;
+          // e.target.src = currentBoardEasy[e.target.id].imageback;
+          document.getElementById(openedPicsEasy[0]).src = currentBoardEasy[openedPicsEasy[0]].imageback;
+          document.getElementById(openedPicsEasy[1]).src = currentBoardEasy[openedPicsEasy[1]].imageback;
             
-            openedPicsEasy.length = 0;
+          openedPicsEasy.length = 0;
           }, 700);
           
           console.log("now opened pics are:" + openedPicsEasy);
           console.log("guessed pics are: " + guessedPicsEasy);
+          //checkWin();
           
           // openedPicsEasy = [];
           //e.target.src = currentBoardEasy[e.target.id].imageback;
       }
+      //checkWin();
+      //if (guessedPicsEasy.length === 16 && easyRemainingTime !== 0) {
+       
+      if (checkWin()) {
+          endGame(true);
+        } 
+      //}
+       //checkWin();
+
+       
         
     }
   });
 });
+
+/*
+function checkWin() {
+  gameOver = false;
+  //return guessedPicsEasy.length;
+  //return easyRemainingTime;
+  if (guessedPicsEasy.length === 16) {
+    gameOver = true;
+    clearInterval(timerHandle);
+    clearTimeout(delayHandle);
+    console.log("checkWin() says: You won!");
+    easyWin++;
+    document.getElementById("easywincounter").textContent = easyWin;
+    document.getElementById("easygamemessage").textContent = "Congratulations! You won!";
+    
+    //break;
+  } else if (guessedPicsEasy.length !== 16) {
+    gameOver = true;
+    clearInterval(timerHandle);
+    clearTimeout(delayHandle);
+    console.log(" checkWin() says: You lost, try again...");
+    easyLoss++;
+    document.getElementById("easylosscounter").textContent = easyLoss;
+    document.getElementById("easygamemessage").textContent = "Sorry... Please try again."
+  }
+  
+}
+*/
+
+
+function checkWin() {
+  gameOver = false;
+  if (guessedPicsEasy.length === 16) {
+    clearInterval(timerHandle);
+    clearTimeout(delayHandle);
+    console.log("checkWin() says: You won!");
+    //easyWin++;
+    //document.getElementById("easywincounter").textContent = easyWin;
+    //document.getElementById("easygamemessage").textContent = "Congratulations! You won!";
+
+  } 
+  else if (easyRemainingTime === 0) {
+    clearInterval(timerHandle);
+    clearTimeout(delayHandle);
+    console.log(" checkWin() says: You lost, try again...");
+    //easyLoss++;
+    //document.getElementById("easylosscounter").textContent = easyLoss;
+    //document.getElementById("easygamemessage").textContent = "Sorry... Please try again."
+  }
+
+}
+
+  /*gameOver = false;
+  if (guessedPicsEasy.length === 16 && easyRemainingTime !== 0) {
+    //gameOver = true;
+    //document.getElementById("easygamemessage").textContent = "Congratulations! You won!";
+    console.log("checkWin says: You won!");
+    //easyWin++;
+    document.getElementById("easywincounter").textContent = easyWin;
+ 
+  } else if (guessedPicsEasy.length < 16 && easyRemainingTime === 0) {
+    //gameOver = true;
+    console.log("checkWin() says: you lost");
+    //document.getElementById("easygamemessage").textContent = "Sorry... Please try again."
+    //easyLoss++;
+    document.getElementById("easylosscounter").textContent = easyLoss;
+  }
+  */
+//}
+
+
+
+function endGame(win) {
+  //if game ends, clear the timers
+  clearTimeout(delayHandle);
+  clearInterval(timerHandle);
+  //change game state to over:
+  gameOver = true;
+  //enable reset button
+  //resetButton.disabled = false;
+  if (win) {
+    //win condition
+    console.log("endGame says: You won!");
+    easyWin++;
+    document.getElementById("easywincounter").textContent = easyWin;
+    document.getElementById("easygamemessage").textContent = "Congratulations! You won!";
+    
+    //change timer colors
+    //timerText.classList.remove("red");
+    //timerText.classList.add("green");
+  } else {
+    //loss condition
+    console.log("endGame says: You lost, try again...");
+    easyLoss++;
+    document.getElementById("easylosscounter").textContent = easyLoss;
+    document.getElementById("easygamemessage").textContent = "Sorry... Please try again."
+    //easyLoss++;
+    //document.getElementById("easylosscounter").textContent = easyLoss;
+    //document.getElementById("easygamemessage").textContent = "Sorry... Please try again."
+
+ 
+    //change background pic
+    //document.body.classList.remove("unexploaded");
+    //document.body.classList.add("exploaded");
+  }
+}
+
+
+function updateClock() {
+  easyRemainingTime--;
+  if (easyRemainingTime <= 0) {
+    endGame(false);
+    //timeLeft = easyRemainingTime;
+  }
+  document.getElementById("easytimer").textContent = easyRemainingTime;
+}
+
+function initGame() {
+  //wiresToCut.length = 0; //empty out the array from a previous game
+  easyRemainingTime = EASY_STARTING_TIME;
+  easyTimerText.textContent = easyRemainingTime;
+  document.getElementById('easygamemessage').textContent = '';
+  guessedPicsEasy.length = 0;
+  //document.getElementById("gridboxeasy").disabled = true;  
+
+  /*
+  for (let box in easyPicsGuessed) {
+    easyPicsGuessed[box] = false;
+  }
+  for (let cell in currentBoardEasy) {
+    currentBoardEasy[cell] = null;
+  }
+  */
+}
+
+function reset() {
+  gameOver = false;
+
+  easyWin = 0;
+  easyLoss = 0;
+
+  document.getElementById("easywincounter").textContent = easyWin;
+  document.getElementById("easylosscounter").textContent = easyLoss;
+  clearTimeout(delayHandle);
+  clearInterval(timerHandle);
+  
+  initGame();
+}
       
